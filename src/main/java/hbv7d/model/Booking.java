60e07c4dbab93646cfe128acb08384c5b3229c72
danielagust <dab47@hi.is>
2025-03-25 15:35:20 +0000

    added all the methods as defaults and all the geters and stetrs

--- src/main/java/hbv7d/model/Booking.java
+++ src/main/java/hbv7d/model/Booking.java
@@ -15,8 +15,47 @@ public class Booking {
         this.status = BookingStatus.PENDING;
     }
 
+    public boolean confirmBooking(){
+        return true;
+    }
 
-    enum BookingStatus {
+    public boolean cancelBooking(){
+        return true;
+    }
+
+    public boolean bookingFaild(){
+        return true;
+    }
+
+    private enum BookingStatus {
         PENDING, CONFIRMED, CANCELLED
+    }
+
+    public BookingStatus getStatus() {
+        return status;
+    }
+
+    public Tour getTour() {
+        return tour;
+    }
+
+    public void setTour(Tour tour) {
+        this.tour = tour;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getBookingID() {
+        return bookingID;
+    }
+
+    public void setBookingID(int bookingID) {
+        this.bookingID = bookingID;
     }
 }
