163897a50715eeeb79d85db29b531b4e0b720607
Halla Svansdottir <hallasvansd@gmail.com>
2025-03-26 14:26:31 +0000

    user.java updateað

--- src/main/java/hbv7d/model/User.java
+++ src/main/java/hbv7d/model/User.java
@@ -9,10 +9,48 @@ public class User {
     private List<Booking> bookings;
 
 
-    
+    // Constructor
     public User(int userId, String name, String email) {
         this.userId = userId;
         this.name = name;
         this.email = email;
+    }
+
+    // GETTERS & SETTERS
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+    
+    // METHODS
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+    }
+
+    public void cancelBooking(Booking booking) {
+        bookings.remove(booking);
     }
 }
